e331bf97aecbc502e81242629cfb3a7b8df1a665
jose luis <cortezluisjose@gmail.com>
2019-08-18 01:20:39 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,17 +1,23 @@
 class Author 
-  attr_accessor :name, :genre 
+  attr_accessor :name, :genre
   @@all = []
   def initialize(name,genre)
     @name=name
     @genre=genre
     save
   end
-  def add_post
+  def add_post(post)
+    @post<<post
+    post.author=self
+    
+    
+  end
+  def post_count 
+    @post
     
     
     
   end
-  
   
   
   
