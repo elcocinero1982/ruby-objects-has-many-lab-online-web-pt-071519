8cd1912cad75d6ee31b50ed786ec8cdbcd88d0ef
jose luis <cortezluisjose@gmail.com>
2019-08-18 01:28:54 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,21 @@
+class Post 
+ attr_accessor :title, :name 
+ @@all=[]
+ 
+ def initialize(title,name)
+  @title=title
+  @name=name
+  @title=[]
+  
+end
+  
+  def sel.all
+    @@all
+  end
+  def author_name
+    
+    
+  end
+  
+  
+end
